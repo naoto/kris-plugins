9b041726009eb46efc5d05843c3f4cc1bc479265
Naoto SHINGAKI <nao.sora+github@gmail.com>
2013-06-19 09:40:00 +0900

    matcher regex update

--- times.rb
+++ times.rb
@@ -2,7 +2,7 @@
 class Times < Kris::Plugin
 
   def on_privmsg(message)
-    if /^((今|いま)(何時|なんじ)|時差)/ =~ message.body
+    if /^((今|いま)(何時|なんじ)|時差)$/ =~ message.body
       japan = Time.now
       us = Time.now.utc - 60 * 60 * 4
       notice(message.to, "日本: #{japan} / NY: #{us}")
