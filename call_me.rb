507dfbdd14c985045e1e860a505fd8715b468417
Naoto SHINGAKI <nao.sora+github@gmail.com>
2013-07-09 17:35:13 +0900

    command add whoami

--- call_me.rb
+++ call_me.rb
@@ -2,7 +2,7 @@
 class CallMe < Kris::Plugin
 
   def on_privmsg(message)
-      if message.body =~ /(\d+)秒後に私の名前を読んで|私は誰/
+      if message.body =~ /(\d+)秒後に私の名前を読んで|私は誰|whoami/
         time = $1
         sleep time.to_i if time != "" && time =~ /\d+/
         privmsg message.to, message.from.gsub(/(\!.+$)/,"") #.gsub(/_/,"")
